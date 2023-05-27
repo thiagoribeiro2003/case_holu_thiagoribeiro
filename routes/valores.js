b016a726e4ef5f8966ee416d2850bbc11f6afc94
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-05-27 18:29:09 -0300

    Organizando o  código

--- routes/valores.js
+++ routes/valores.js
@@ -6,22 +6,97 @@ const app = express();
 app.use(express.json());
 
 
+/*              VALORES        */
 
-// Consulta todos os valores
+// Consulta todos os valores e resultados
 app.get("/calculo", (req, res) => {
-    conexao.query("SELECT * FROM valores", (err, results) => {
-        if(err) {
-            console.log("Erro ao executar a consulta:", err);
-            res.status(500).json({ error: "Erro ao consultar registros"})
-            return
-        }
-        res.json(results)
-    })
-  })
+  const query = `
+    SELECT valores.*, resultados.*
+    FROM valores
+    JOIN resultados ON valores.id = resultados.id
+  `;
+
+  conexao.query(query, (err, results) => {
+    if (err) {
+      console.error("Erro ao executar a consulta:", err);
+      res.status(500).json({ error: "Erro ao consultar registros" });
+      return;
+    }
+
+    const data = results.map((row) => ({
+      valores: {
+        id: row.id,
+        potenciaTotalParametroKw: row.potenciaTotalParametroKw,
+        potenciaTotalPainelW: row.potenciaTotalPainelW,
+        alturaPainel: row.alturaPainel,
+        larguraPainel: row.larguraPainel,
+        maximoPainelPorMicroInversor: row.maximoPainelPorMicroInversor,
+      },
+      resultados: {
+        id: row.id,
+        quantidadeDePaineis: row.quantidadeDePaineis,
+        quantidadeMicroInversores: row.quantidadeMicroInversores,
+        quantidadeColunas: row.quantidadeColunas,
+        comprimentoDaEstrutura: row.comprimentoDaEstrutura,
+        alturaDaEstrutura: row.alturaDaEstrutura,
+        areaUtil: row.areaUtil,
+      },
+    }));
+
+    res.json(data);
+  });
+});
+
+// Consulta valores e resultados específicos com base no ID
+app.get("/calculo/:id", (req, res) => {
+  const id = req.params.id;
+
+  const query = `
+    SELECT valores.*, resultados.*
+    FROM valores
+    JOIN resultados ON valores.id = resultados.id
+    WHERE valores.id = ?
+  `;
+
+  conexao.query(query, [id], (err, results) => {
+    if (err) {
+      console.error("Erro ao executar a consulta:", err);
+      res.status(500).json({ error: "Erro ao consultar registros" });
+      return;
+    }
+
+    if (results.length === 0) {
+      res.status(404).json({ error: "Cálculo não encontrado" });
+      return;
+    }
+
+    const data = results.map((row) => ({
+      valores: {
+        id: row.id,
+        potenciaTotalParametroKw: row.potenciaTotalParametroKw,
+        potenciaTotalPainel: row.potenciaTotalPainel,
+        alturaPainel: row.alturaPainel,
+        larguraPainel: row.larguraPainel,
+        maximoPainelPorMicroInversor: row.maximoPainelPorMicroInversor,
+      },
+      resultados: {
+        id: row.id,
+        quantidadeDePaineis: row.quantidadeDePaineis,
+        quantidadeMicroInversores: row.quantidadeMicroInversores,
+        quantidadeColunas: row.quantidadeColunas,
+        comprimentoDaEstrutura: row.comprimentoDaEstrutura,
+        alturaDaEstrutura: row.alturaDaEstrutura,
+        areaUtil: row.areaUtil,
+      },
+    }));
+
+    res.json(data[0]); // Retorna apenas o primeiro resultado encontrado
+  });
+});
 
 
-// Inserção de valores e cálculo dos resultados
 
+// Insere os valores e com base neles enviam os resultados
 app.post("/calculo", (req, res) => {
   const {
     potenciaTotalParametroKw,
@@ -49,7 +124,7 @@ app.post("/calculo", (req, res) => {
         return;
       }
 
-      const id_valores = result.insertId;
+      const id = result.insertId;
 
       // Calcular os valores adicionais
       const quantidadeDePaineis = Math.ceil(
@@ -65,11 +140,10 @@ app.post("/calculo", (req, res) => {
 
       // Inserir os valores calculados na tabela "resultados"
       const resultadosQuery =
-        "INSERT INTO resultados (id_valores, quantidadeDePaineis, quantidadeMicroInversores, quantidadeColunas, comprimentoDaEstrutura, alturaDaEstrutura, areaUtil) VALUES (?,?,?,?,?,?,?)";
+        "INSERT INTO resultados (quantidadeDePaineis, quantidadeMicroInversores, quantidadeColunas, comprimentoDaEstrutura, alturaDaEstrutura, areaUtil) VALUES (?,?,?,?,?,?)";
       conexao.query(
         resultadosQuery,
         [
-          id_valores,
           quantidadeDePaineis,
           quantidadeMicroInversores,
           quantidadeColunas,
@@ -92,48 +166,151 @@ app.post("/calculo", (req, res) => {
   );
 });
 
+// Altera os valores de um calculo e muda seus resultados com base num ID específico
+app.patch("/calculo/:id", (req, res) => {
+  const id = req.params.id;
+
+  const {
+    potenciaTotalParametroKw,
+    potenciaTotalPainelW,
+    alturaPainel,
+    larguraPainel,
+    maximoPainelPorMicroInversor,
+  } = req.body;
+
+  // Atualizar os valores na tabela "valores"
+  const updateValoresQuery = `
+    UPDATE valores
+    SET potenciaTotalParametroKw = ?,
+        potenciaTotalPainelW = ?,
+        alturaPainel = ?,
+        larguraPainel = ?,
+        maximoPainelPorMicroInversor = ?
+    WHERE id = ?
+  `;
+
+  conexao.query(
+    updateValoresQuery,
+    [
+      potenciaTotalParametroKw,
+      potenciaTotalPainelW,
+      alturaPainel,
+      larguraPainel,
+      maximoPainelPorMicroInversor,
+      id,
+    ],
+    (err, result) => {
+      if (err) {
+        console.error("Erro ao atualizar valores:", err);
+        res.status(500).json({ error: "Erro ao atualizar valores" });
+        return;
+      }
+
+      // Calcular os novos valores
+      const quantidadeDePaineis = Math.ceil(
+        (potenciaTotalParametroKw * 1000) / potenciaTotalPainelW
+      );
+      const quantidadeMicroInversores = Math.ceil(
+        quantidadeDePaineis / maximoPainelPorMicroInversor
+      );
+      const quantidadeColunas = Math.ceil(quantidadeDePaineis / 2);
+      const comprimentoDaEstrutura = quantidadeColunas * larguraPainel;
+      const alturaDaEstrutura = 2 * alturaPainel;
+      const areaUtil = comprimentoDaEstrutura * alturaDaEstrutura;
+
+      // Atualizar os resultados na tabela "resultados"
+      const updateResultadosQuery = `
+        UPDATE resultados
+        SET quantidadeDePaineis = ?,
+            quantidadeMicroInversores = ?,
+            quantidadeColunas = ?,
+            comprimentoDaEstrutura = ?,
+            alturaDaEstrutura = ?,
+            areaUtil = ?
+        WHERE id = ?
+      `;
+
+      conexao.query(
+        updateResultadosQuery,
+        [
+          quantidadeDePaineis,
+          quantidadeMicroInversores,
+          quantidadeColunas,
+          comprimentoDaEstrutura,
+          alturaDaEstrutura,
+          areaUtil,
+          id,
+        ],
+        (err, result) => {
+          if (err) {
+            console.error("Erro ao atualizar resultados:", err);
+            res.status(500).json({ error: "Erro ao atualizar resultados" });
+            return;
+          }
+          res.json({ message: "Valores e resultados atualizados com sucesso" });
+        }
+      );
+    }
+  );
+});
 
 
+// Exclui todos os valores e resultados do banco 
+app.delete("/calculo", (req, res) => {
+  // Excluir todos os registros da tabela "resultados"
+  const resultadosQuery = "DELETE FROM resultados";
+
+  conexao.query(resultadosQuery, (err, result) => {
+    if (err) {
+      console.error("Erro ao excluir registros da tabela 'resultados':", err);
+      res.status(500).json({ error: "Erro ao excluir registros" });
+      return;
+    }
+
+    // Excluir todos os registros da tabela "valores"
+    const valoresQuery = "DELETE FROM valores";
+    conexao.query(valoresQuery, (err, result) => {
+      if (err) {
+        console.error("Erro ao excluir registros da tabela 'valores':", err);
+        res.status(500).json({ error: "Erro ao excluir registros" });
+        return;
+      }
+
+      res.json({ message: "Registros excluídos com sucesso" });
+    });
+  });
+});
 
 
+// Exclui um calculo expecífico (junto com seus resultados)
+app.delete("/calculo/:id", (req, res) => {
+  const id = req.params.id;
 
+  // Excluir o registro da tabela "resultados" usando o id_resultados correspondente na tabela "valores"
+  const resultadosQuery = "DELETE FROM resultados WHERE id = (SELECT id FROM valores WHERE id = ?)";
 
+  conexao.query(resultadosQuery, [id], (err, result) => {
+    if (err) {
+      console.error("Erro ao excluir registro da tabela 'resultados':", err);
+      res.status(500).json({ error: "Erro ao excluir registro" });
+      return;
+    }
+
+    // Excluir o registro da tabela "valores"
+    const valoresQuery = "DELETE FROM valores WHERE id = ?";
+    conexao.query(valoresQuery, [id], (err, result) => {
+      if (err) {
+        console.error("Erro ao excluir registro da tabela 'valores':", err);
+        res.status(500).json({ error: "Erro ao excluir registro" });
+        return;
+      }
+
+      res.json({ message: "Registro excluído com sucesso" });
+    });
+  });
+});
 
 /* configurando o servidor */
 app.listen(3000, () => {
   console.log("Servidor Express iniciado na porta 3000");
 });
-
-
-/*
-const potenciaTotalParametroKw = 4.5;
-const potenciaTotalPainelW = 550;
-const alturaPainel = 1.95;
-const larguraPainel = 1.1;
-const maximoPainelPorMicroInversor = 4;
-
-function descobrindoValores(potenciaTotalParametroKw){
-  const quantidadeDePaineis = Math.ceil(potenciaTotalParametroKw * 1000 / potenciaTotalPainelW);
-  const quantidadeMicroInversores =  Math.ceil(quantidadeDePaineis / maximoPainelPorMicroInversor);
-  const quantidadeDeColunas = Math.ceil(quantidadeDePaineis / 2);                          
-  const comprimentoDaEstrutura = quantidadeDeColunas * larguraPainel;
-  const alturaDaEstrutura = 2 * alturaPainel; // 2 - Quantidade de linhas (pois será uma linha com 5 paineis e outra com 4 paineis)
-  const areaUtil = comprimentoDaEstrutura * alturaDaEstrutura
-
-  return{
-    quantidadeDePaineis,
-    quantidadeMicroInversores,
-    quantidadeDeColunas,
-    comprimentoDaEstrutura,
-    alturaDaEstrutura,
-    areaUtil
-  } 
-}
-
-const resultado = descobrindoValores(potenciaTotalParametroKw);
-
-console.log(`Quantidade de painéis necessários: ${resultado.quantidadeDePaineis}`);
-console.log(`Quantidade de micro inversores necessários: ${resultado.quantidadeMicroInversores}`);
-console.log(`Comprimento da estrutura necessária: ${resultado.comprimentoDaEstrutura}m`);
-console.log(`Área útil nescessária: ${resultado.areaUtil}m²`);
-*/
